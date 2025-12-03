3622cf628cce14d8a12eac6e0987e9f1b14be708
Arvind <arvkrish@redhat.com>
2025-12-03 12:14:15 +0530

    updated loading component to use spinner and used it in pipelineoverview page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
+import { Spinner, SpinnerProps } from '@patternfly/react-core';
 import classNames from 'classnames';
 
 type LoadingProps = {
   className?: string;
+  isInline?: boolean;
+  size?: SpinnerProps["size"];
+  ariaLabel?: string;
 };
 
-export const Loading: React.FC<LoadingProps> = ({ className }) => (
+export const Loading: React.FC<LoadingProps> = ({ className, isInline, size, ariaLabel }) => (
   <div
-    className={classNames('co-m-loader co-an-fade-in-out', className)}
+    className= {classNames(
+      'pf-v5-u-display-flex', 
+      'pf-v5-u-justify-content-center',
+      'pf-v5-u-align-items-center',
+      'pf-v5-u-w-100',
+      'pf-v5-u-h-100',
+      className)}
     data-test="loading-indicator"
   >
-    <div className="co-m-loader-dot__one" />
-    <div className="co-m-loader-dot__two" />
-    <div className="co-m-loader-dot__three" />
+    <Spinner 
+     size={size}
+     isInline={isInline}
+     aria-label={ariaLabel}
+    />
   </div>
 );
 Loading.displayName = 'Loading';
 
 export const LoadingInline: React.FC = () => (
-  <Loading className="co-m-loader--inline" />
+  <Loading isInline = {true} />
 );
 LoadingInline.displayName = 'LoadingInline';
